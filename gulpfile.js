068844de504c671d5f848f7306fa47e959214443
neripark <neripark@gmail.com>
2018-02-22 01:08:58 +0900

    watch OK

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var gulpWebserver = require('gulp-webserver');
 var ejs = require('gulp-ejs');
+var rename = require('gulp-rename');
 
 gulp.task('ejs',function(){
   gulp.src('./src/*.ejs')
     .pipe(ejs())
+    .pipe(rename({extname: '.html'}))
     .pipe(gulp.dest('./dist'));
 });
 
@@ -17,4 +19,10 @@ gulp.task('webserver', function(){
     }));
 });
 
-gulp.task('default', ['webserver','ejs']);
+gulp.task('watch', function(){
+  gulp.watch('src/**/*.ejs', ['ejs']);
+});
+
+gulp
+
+gulp.task('default', ['webserver','ejs','watch']);
