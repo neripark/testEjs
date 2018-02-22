7163177d2ee174a328240194282d9405640e8d3f
neripark <neripark@gmail.com>
2018-02-22 23:32:29 +0900

    sass導入

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,20 @@ var gulpWebserver = require('gulp-webserver');
 var ejs = require('gulp-ejs');
 var rename = require('gulp-rename');
 var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
 
 gulp.task('ejs',function(){
   gulp.src('./src/*.ejs')
     .pipe(plumber())
     .pipe(ejs())
     .pipe(rename({extname: '.html'}))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('sass',function(){
+  gulp.src('./src/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass({outputStyle: 'compact'}))
     .pipe(gulp.dest('./dist'));
 });
 
@@ -23,6 +31,7 @@ gulp.task('webserver', function(){
 
 gulp.task('watch', function(){
   gulp.watch('src/**/*.ejs', ['ejs']);
+  gulp.watch('src/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['webserver','ejs','watch']);
+gulp.task('default', ['webserver','ejs','sass','watch']);
